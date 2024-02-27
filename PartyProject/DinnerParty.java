b795da9864030e44b20eaf5abe3fdcdea34e3eb4
Kyan L.C <157541674+kyabetes@users.noreply.github.com>
2024-02-27 15:45:19 -0500

    Update DinnerParty.java

--- PartyProject/DinnerParty.java
+++ PartyProject/DinnerParty.java
@@ -1,13 +1,43 @@
-public class DinnerParty extends Party {
-    private int dinnerChoice; // Additional data field for DinnerParty
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    // Method to set the dinner menu choice
+public class DinnerParty extends Party {
+    private int dinnerChoice;
+    private ArrayList<Integer> ratings;
+
+    public DinnerParty() {
+        super();
+        ratings = new ArrayList<>();
+    }
+
     public void setDinnerChoice(int choice) {
         dinnerChoice = choice;
     }
 
-    // Method to get the dinner menu choice
     public int getDinnerChoice() {
         return dinnerChoice;
+    }
+
+    public void collectRatings() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Collecting ratings from guests (Enter -1 to end):");
+        while (true) {
+            int rating = scanner.nextInt();
+            if (rating == -1) {
+                break;
+            }
+            ratings.add(rating);
+        }
+    }
+
+    public double calculateAverageRating() {
+        if (ratings.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        for (int rating : ratings) {
+            sum += rating;
+        }
+        return (double) sum / ratings.size();
     }
 }
