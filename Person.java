8b425b5645d7816083ed4bef642517996593e2f3
Kyan L.C <157541674+kyabetes@users.noreply.github.com>
2024-02-27 15:25:39 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    // constructor
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // get methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
